2cf34b8f6d2aa094a67cdd614344e18273a0d06d
ryan-singleton <ryan@northofreal.com>
2026-03-15 13:03:04 -0700

    Remove the move operation from the build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,9 +4,7 @@ mkdir -p "${STEAMAPPDIR}" || true
 bash "${STEAMCMDDIR}"/steamcmd.sh \
     +force_install_dir "${STEAMAPPDIR}" \
     +login anonymous \
-    +app_update "${STEAMAPPID}" \
-    +validate +quit
+    +app_update "${STEAMAPPID}" validate \
+    +quit
 
-mv -f "${HOMEDIR}"/start-server.sh "${STEAMAPPDIR}"/start-server.sh
-
-cd "${STEAMAPPDIR}" && ./start-server.sh
+cd "${STEAMAPPDIR}" && bash "${HOMEDIR}/start-server.sh"
